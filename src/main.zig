8ae95000db7fcbc3aee34f412586f696eb6625f3
Nathan Craddock <nzcraddock@gmail.com>
2022-01-08 14:16:43 -0700

    feat: add command line arg parsing
    Adds basic command line argument parsing for:
    * -h, --help
    * -q, --query
    
    zf will remain very focused, so there isn't a need for a more robust
    parser. This simple one will do just fine.

--- src/main.zig
+++ src/main.zig
@@ -7,31 +7,96 @@ const ArrayList = std.ArrayList;
 const filter = @import("filter.zig");
 const ui = @import("ui.zig");
 
+const version = "0.1";
+
+const help =
+    \\Usage: zf [OPTION]...
+    \\-q, --query   Filter using the given query
+    \\-h, --help    Display this help and exit
+;
+
+const Config = struct {
+    help: bool = false,
+    skip_ui: bool = false,
+    query: []u8 = undefined,
+};
+
+fn parseArgs(allocator: std.mem.Allocator) !Config {
+    const stderr = std.io.getStdErr().writer();
+    const args = try std.process.argsAlloc(allocator);
+
+    var config: Config = .{};
+
+    const eql = std.mem.eql;
+    var skip = false;
+    for (args[1..]) |arg, i| {
+        if (skip) continue;
+        const index = i + 1;
+
+        if (eql(u8, arg, "-h") or eql(u8, arg, "--help")) {
+            config.help = true;
+        } else if (eql(u8, arg, "-q") or eql(u8, arg, "--query")) {
+            config.skip_ui = true;
+
+            // read query
+            if (index + 1 > args.len - 1) {
+                try stderr.print("zf: option '{s}' requires an argument\n", .{arg});
+                try stderr.print("{s}\n", .{help});
+                std.process.exit(1);
+            }
+
+            config.query = try allocator.alloc(u8, args[index + 1].len);
+            std.mem.copy(u8, config.query, args[index + 1]);
+            skip = true;
+        } else {
+            try stderr.print("zf: unrecognized option '{s}'\n", .{arg});
+            try stderr.print("{s}\n", .{help});
+            std.process.exit(1);
+        }
+    }
+
+    return config;
+}
+
 pub fn main() anyerror!void {
-    // create an arena allocator to reduce time spent allocating and freeing memory during runtime
+    // create an arena allocator to reduce time spent allocating
+    // and freeing memory during runtime.
     var arena = heap.ArenaAllocator.init(heap.page_allocator);
     defer arena.deinit();
 
+    const stdout = std.io.getStdOut().writer();
     const allocator = arena.allocator();
 
-    // TODO: read cmd args
+    const config = try parseArgs(allocator);
+    _ = config;
+
+    // help ignores all other args and quits
+    if (config.help) {
+        try stdout.print("{s}\n", .{help});
+        return;
+    }
 
     // read all lines or exit on out of memory
     var stdin = io.getStdIn().reader();
-    var buf = try readAll(allocator, &stdin);
+    const buf = try readAll(allocator, &stdin);
 
     const delimiter = '\n';
     var candidates = try filter.collectCandidates(allocator, buf, delimiter);
 
-    var terminal = try ui.Terminal.init();
-    var selected = try ui.run(allocator, &terminal, candidates);
-    try ui.cleanUp(&terminal);
-    terminal.deinit();
+    if (config.skip_ui) {
+        for (try filter.rankCandidates(allocator, candidates, config.query)) |candidate| {
+            try stdout.print("{s}\n", .{candidate.str});
+        }
+    } else {
+        var terminal = try ui.Terminal.init();
+        var selected = try ui.run(allocator, &terminal, candidates);
+        try ui.cleanUp(&terminal);
+        terminal.deinit();
 
-    if (selected) |res| {
-        defer res.deinit();
-        const stdout = std.io.getStdOut().writer();
-        try stdout.print("{s}\n", .{res.items});
+        if (selected) |result| {
+            defer result.deinit();
+            try stdout.print("{s}\n", .{result.items});
+        }
     }
 }
 
